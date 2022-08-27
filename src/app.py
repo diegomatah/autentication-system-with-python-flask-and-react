b3a93906dec4c3e4d8e1bb32e396a20dd453ea98
diegomatah <diegoa.matah@gmail.com>
2022-08-27 19:06:56 +0000

    tarea

--- src/app.py
+++ src/app.py
@@ -7,10 +7,11 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db,User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 #from models import Person
 
@@ -18,7 +19,7 @@ ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+jwt = JWTManager(app)
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -62,6 +63,33 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0 # avoid cache memory
     return response
+
+@app.route('/private', methods=['POST'])
+@jwt_required()
+def private():
+    identidad = get_jwt_identity()
+
+
+    return jsonify({"mensaje":"Bienvenido "+ identidad})
+
+    
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    user = User.query.filter_by(email=body['email']).first()
+    if(user):
+        if(user.password==body['password']):
+            expiracion = datetime.timedelta(minutes=1)
+            token1 = create_access_token(identity=body['email'],expires_delta = expiracion)
+            return jsonify({
+        "email": body ['email'],
+        "mensaje":"Bienvenido",
+        "token": token1
+    })
+        else:
+            return jsonify({"mensaje":'usuario o clave incorrecta'})  
+    else:
+       return jsonify({"mensaje":'El usuario no existe'})
 
 
 # this only runs if `$ python src/main.py` is executed
